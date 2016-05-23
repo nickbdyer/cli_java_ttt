fee46dfa7c1db7977b70990dc517c92c7f13577d
Nick Dyer <nbdyer@gmail.com>
2016-05-23 21:32:30 +0100

    Adds tests extracted from game class into gameengine for cli package

--- src/test/java/uk/nickbdyer/cli_ttt/GameEngineTest.java
+++ src/test/java/uk/nickbdyer/cli_ttt/GameEngineTest.java
@@ -1,5 +1,6 @@
 package uk.nickbdyer.cli_ttt;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -10,79 +11,70 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 
 public class GameEngineTest {
+
+    private GameEngine gameEngine;
+    private ByteArrayOutputStream outContent;
+    private CLI ui;
+
+    @Before
+    public void setUp() {
+        gameEngine = new GameEngine();
+        outContent = new ByteArrayOutputStream();
+    }
     
     @Test
     public void canPlayGameThrough() {
-        GameEngine engine = new GameEngine();
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        CLI ui = new CLI(new Scanner("1 1 2 4 5 7 n"), new PrintStream(outContent));
-        engine.start(ui);
+        ui = new CLI(new Scanner("1 1 2 4 5 7 n"), new PrintStream(outContent));
+        gameEngine.start(ui);
         assertThat(outContent.toString(), containsString("Would you like to play again (y/n)?"));
     }
 
     @Test
     public void canPlayGameThroughTwice() {
-        GameEngine engine = new GameEngine();
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        CLI ui = new CLI(new Scanner("1 1 2 4 5 7 y 1 1 4 2 5 3 n"), new PrintStream(outContent));
-        engine.start(ui);
+        ui = new CLI(new Scanner("1 1 2 4 5 7 y 1 1 4 2 5 3 n"), new PrintStream(outContent));
+        gameEngine.start(ui);
         assertThat(outContent.toString(), containsString("Resetting game board!"));
     }
 
+    @Test
+    public void gameWillCallDrawGameOverStatement() {
+        ui = new CLI(new Scanner("1 1 2 4 5 8 7 3 6 9 n"), new PrintStream(outContent));
+        gameEngine.start(ui);
+        assertThat(outContent.toString(), containsString("It's a Draw!"));
+    }
 
-//    @Test
-//    public void gameWillCallDrawGameOverStatement() {
-//        makeMultipleMoves(9, "1 2 4 5 8 7 3 6 9");
-//        game.endGame(board,ui);
-//        assertThat(outContent.toString(), containsString("It's a Draw!"));
-//    }
-//
-//    @Test
-//    public void gameWillCallWinGameOverStatement() {
-//        makeMultipleMoves(5, "1 2 4 5 7");
-//        game.endGame(board,ui);
-//        assertThat(outContent.toString(), containsString("X has won!"));
-//    }
-//
-//    @Test
-//    public void gameKnowsWhosTurnItIs() {
-//        makeMultipleMoves(5, "1 2 4 5 7");
-//        assertEquals(X, board.getMarkAt(0));
-//        assertEquals(O, board.getMarkAt(1));
-//    }
-//
-//    @Test
-//    public void willRejectOutOfBoundsEntry() {
-//        makeMultipleMoves(5, "23 1 4 2 5 3");
-//        game.endGame(board, ui);
-//        assertThat(outContent.toString(), containsString("That is not a valid position"));
-//    }
-//
-//    @Test
-//    public void willNotAllowAMarkedCellToBeMarked() {
-//        makeMultipleMoves(5, "1 1 4 2 5 3");
-//        game.endGame(board, ui);
-//        assertThat(outContent.toString(), containsString("That is not a valid position"));
-//    }
-//
-//    @Test
-//    public void willShowWinningMessageX() {
-//        makeMultipleMoves(5, "1 4 2 5 3");
-//        game.endGame(board, ui);
-//        assertThat(outContent.toString(), containsString("X has won!"));
-//    }
-//
-//    @Test
-//    public void willShowWinningMessageO() {
-//        makeMultipleMoves(6, "1 4 2 5 8 6");
-//        game.endGame(board, ui);
-//        assertThat(outContent.toString(), containsString("O has won!"));
-//    }
-//
-//    private void makeMultipleMoves(int numberOfMoves, String positions) {
-//        game = new Game(new PlayerFactory().create(GameType.HvsH));
-//        for (int i = 0; i < numberOfMoves; i++) {
-//            game.promptTurn(board, ui);
-//        }
-//    }
+    @Test
+    public void gameWillCallWinGameOverStatement() {
+        ui = new CLI(new Scanner("1 1 2 4 5 7 n"), new PrintStream(outContent));
+        gameEngine.start(ui);
+        assertThat(outContent.toString(), containsString("X has won!"));
+    }
+
+    @Test
+    public void willRejectOutOfBoundsEntry() {
+        ui = new CLI(new Scanner("1 23 1 4 2 5 3 n"), new PrintStream(outContent));
+        gameEngine.start(ui);
+        assertThat(outContent.toString(), containsString("That is not a valid position"));
+    }
+
+    @Test
+    public void willNotAllowAMarkedCellToBeMarked() {
+        ui = new CLI(new Scanner("1 1 1 4 2 5 3 n"), new PrintStream(outContent));
+        gameEngine.start(ui);
+        assertThat(outContent.toString(), containsString("That is not a valid position"));
+    }
+
+    @Test
+    public void willShowWinningMessageX() {
+        ui = new CLI(new Scanner("1 1 4 2 5 3 n"), new PrintStream(outContent));
+        gameEngine.start(ui);
+        assertThat(outContent.toString(), containsString("X has won!"));
+    }
+
+    @Test
+    public void willShowWinningMessageO() {
+        ui = new CLI(new Scanner("1 1 4 2 5 8 6 n"), new PrintStream(outContent));
+        gameEngine.start(ui);
+        assertThat(outContent.toString(), containsString("O has won!"));
+    }
 }
